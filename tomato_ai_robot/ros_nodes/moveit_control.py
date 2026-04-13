8c9c6e195e5f51fe11d32da1974a8209e8d4f25f
Ahmed <tanvirahmed754575@gmail.com>
2026-04-13 18:03:36 +0300

    arm command

--- tomato_ai_robot/ros_nodes/moveit_control.py
+++ tomato_ai_robot/ros_nodes/moveit_control.py
@@ -1,21 +1,3 @@
-# ros_nodes/moveit_control.py
-# import moveit_commander
-
-# class RobotArm:
-
-#     def __init__(self):
-#         moveit_commander.roscpp_initialize([])
-#         self.arm = moveit_commander.MoveGroupCommander("manipulator")
-
-#     def go_to(self, pos):
-#         pose = self.arm.get_current_pose().pose
-
-#         pose.position.x = pos[0]
-#         pose.position.y = pos[1]
-#         pose.position.z = pos[2]
-
-#         self.arm.set_pose_target(pose)
-#         self.arm.go()
 
 
 class RobotArm:
