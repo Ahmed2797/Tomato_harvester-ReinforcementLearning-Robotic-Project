90e313228c22cb7f5798b73b28a88a42fa952cd9
Ahmed <tanvirahmed754575@gmail.com>
2026-04-13 15:48:01 +0300

    stracture

---
+++ templates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create project structure
+
+mkdir -p tomato_ai_robot/models
+
+mkdir -p tomato_ai_robot/src
+mkdir -p tomato_ai_robot/src/__init__.py
+
+# Create source files
+
+# Create main file
+touch tomato_ai_robot/main.py
+
+echo "✅ Project structure created successfully!"
